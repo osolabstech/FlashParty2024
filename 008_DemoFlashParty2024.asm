0291d71d6f4ee453c8dca996911848e5cf0a423d
carlinhocr <carlos.rienzi@gmail.com>
2024-10-02 12:22:51 -0300

    Adding Scroll

--- 008_DemoFlashParty2024.asm
+++ 008_DemoFlashParty2024.asm
@@ -90,6 +90,7 @@ pscroll_l2_high=$C6
 pscroll_l3_low=$C7
 pscroll_l3_high=$C8
 max_columns_scroll=$c9
+columns_counter=$ca
 
 
 ;define LCD signals
@@ -410,7 +411,8 @@ scroller_start:
   jsr clear_display
   lda #$70 ;max columns scroll
   sta max_columns_scroll
-  ldx #$FF ; to start al column 0
+  lda #$00
+  sta columns_counter
   lda #<scroll_linea_0
   sta pscroll_l0_low
   lda #>scroll_linea_0
@@ -435,7 +437,8 @@ print_scroll_screen_loop:
   jsr print_scroll_l1
   jsr print_scroll_l2
   jsr print_scroll_l3
-  cpx max_columns_scroll ;end all when we are at the last column of line 3
+  lda columns_counter
+  cmp max_columns_scroll ;end all when we are at the last column of line 3
   beq print_scroll_end
   jsr DELAY_HALF_SEC ; delay half a second to see the effect
   jsr inc_all_pscroll_lines
@@ -486,12 +489,13 @@ print_scroll_l3:
   ldy #$FF
 print_scroll_loop_l3:
   ;print line zero 20 chars
-  inx ;keep only here to count only 20 columns and not one column per line
+  inc columns_counter ;keep only here to count only 20 columns per screen and not one column per line
       ;as it would be if I keep it on l1, l2 and l0
   iny
   lda (pscroll_l3_low),y
   jsr print_char 
-  cpx max_columns_scroll ;end all when we are at the last column of line 3
+  lda columns_counter
+  cmp max_columns_scroll ;end all when we are at the last column of line 3
   beq print_scroll_end
   cpy #$13 ;19 20 decimal characters printed on the line from 0 to 19
   bne print_scroll_loop_l3
