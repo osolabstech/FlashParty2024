a336cafd1bb9667600937194c2406c01353f1d68
carlinhocr <carlos.rienzi@gmail.com>
2024-10-02 11:58:32 -0300

    Adding Scroll

--- 008_DemoFlashParty2024.asm
+++ 008_DemoFlashParty2024.asm
@@ -410,7 +410,7 @@ scroller_start:
   jsr clear_display
   jsr test_custom_chars
   jsr delay_5_sec
-  lda #$23 ;max columns scroll
+  lda #$70 ;max columns scroll
   sta max_columns_scroll
   ldx #$FF ; to start al column 0
   lda #<scroll_linea_0
@@ -450,7 +450,7 @@ print_scroll_loop_l0:
   iny
   lda (pscroll_l0_low),y
   jsr print_char 
-  cpy #$14 ;20 decimal characters printed on the line
+  cpy #$13 ;20 decimal characters printed on the line
   bne print_scroll_loop_l0
   rts
 
@@ -463,7 +463,7 @@ print_scroll_loop_l1:
   iny
   lda (pscroll_l1_low),y
   jsr print_char 
-  cpy #$14 ;19 20 decimal characters printed on the line from 0 to 19
+  cpy #$13 ;19 20 decimal characters printed on the line from 0 to 19
   bne print_scroll_loop_l1
   rts
 
@@ -476,7 +476,7 @@ print_scroll_loop_l2:
   iny
   lda (pscroll_l2_low),y
   jsr print_char 
-  cpy #$14 ;19 20 decimal characters printed on the line from 0 to 19
+  cpy #$13 ;19 20 decimal characters printed on the line from 0 to 19
   bne print_scroll_loop_l2
   rts
 
@@ -493,7 +493,7 @@ print_scroll_loop_l3:
   jsr print_char 
   cpx max_columns_scroll ;end all when we are at the last column of line 3
   beq print_scroll_end
-  cpy #$14 ;19 20 decimal characters printed on the line from 0 to 19
+  cpy #$13 ;19 20 decimal characters printed on the line from 0 to 19
   bne print_scroll_loop_l3
   rts
 
